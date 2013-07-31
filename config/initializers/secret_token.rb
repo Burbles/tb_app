60596a4734b8e9b30d0c888dbb1be91b74a763a7
Burbles <burbinator@gmail.com>
2013-07-31 03:31:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TbApp::Application.config.secret_token = '8dcb576968ff6b6ed0c23f7ee01903ede0171f5847c8bca1414b50d874f0db8d4ff2f93c320c545555ff64be4927dbe15628bde61e1f34904c34ff19c3f90f7c'
